a1b934b6453d5162d4610f0f121a16d4ef4ce86d
Jesus_Alvarado-Huayhuaz <inefable12@gmail.com>
2025-02-05 23:03:52 -0500

    Update app.py

--- app.py
+++ app.py
@@ -1,13 +1,8 @@
 import streamlit as st
-import pandas as pd
-
-def process_file(uploaded_file):
-    if uploaded_file is not None:
-        df = pd.read_excel(uploaded_file, skiprows=4)  # La fila 5 es índice 4 en Python
-        total_pago = df[df["Tipo de Transacción"] == "TE PAGÓ"]["Monto"].sum()
-        st.write("### Total de Monto para 'TE PAGÓ':", total_pago)
+import pandas as pd  
 
 st.title("REPORTE YAPE")
-
 uploaded_file = st.file_uploader("Sube un archivo Excel", type=["xlsx"])
-process_file(uploaded_file)
+df = pd.read_excel(uploaded_file, skiprows=4)
+monto_te_pago = df[df["Tipo de Transacción"] == "TE PAGÓ"]["Monto"].sum()
+st.write("### Total de Monto para 'TE PAGÓ':", monto_te_pago)
