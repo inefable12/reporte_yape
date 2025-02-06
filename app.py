b8f5d7bac3c4a30c42658471b208d0456d2fd066
Jesus_Alvarado-Huayhuaz <inefable12@gmail.com>
2025-02-05 23:16:17 -0500

    Update app.py

--- app.py
+++ app.py
@@ -3,6 +3,18 @@ import pandas as pd
 
 st.title("REPORTE YAPE")
 uploaded_file = st.file_uploader("Sube un archivo Excel", type=["xlsx"])
-df = pd.read_excel(uploaded_file, skiprows=4)
+df = pd.read_excel(uploaded_file)
+df = df.iloc[4:]
+new_column_names = {
+    df.columns[0]: "Tipo de Transacción",
+    df.columns[1]: "Origen",
+    df.columns[2]: "Destino",
+    df.columns[3]: "Monto",
+    df.columns[4]: "Mensaje",
+    df.columns[5]: "Fecha de operación"
+}
+df = df.rename(columns=new_column_names)
+df["Monto"] = pd.to_numeric(df["Monto"], errors='coerce')
+df["Monto"] = df["Monto"].astype(float)
 monto_te_pago = df[df["Tipo de Transacción"] == "TE PAGÓ"]["Monto"].sum()
 st.write("### Total de Monto para 'TE PAGÓ':", monto_te_pago)
